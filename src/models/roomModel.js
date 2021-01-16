6db6cc454558faa5571e33234155029ad0845579
LAPTOP-C5K5KGGH\stephanie.park <stephanie.p@waterloop.ca>
2021-01-16 18:44:46 -0500

    debug

--- src/models/roomModel.js
+++ src/models/roomModel.js
@@ -1,9 +1,11 @@
+const mongoose = require("mongoose");
+
 const roomSchema = new mongoose.Schema({
     roomid: { type: String, required: true },
     users: { type: [String], required: true },
     accepted: { type: [String], required: true },
   });
   
-  const User = mongoose.model("rooms", roomSchema);
+const User = mongoose.model("rooms", roomSchema);
   
-  module.exports = User;
+module.exports = User;
