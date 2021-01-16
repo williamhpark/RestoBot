7f4dfefb028b398295b3d9bd42b9491c78eddc7a
William Park <william.h.park@gmail.com>
2021-01-16 14:42:47 -0800

    updates

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -7,6 +7,7 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 let postWebhook = (req, res) => {
   // Parse the request body from the POST
+  // to delete
   let body = req.body;
 
   // Check the webhook event is from a Page subscription
