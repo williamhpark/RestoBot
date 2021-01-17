a85ca209cfc1d7cca6d92f5d56b43e56cdfe2e50
William Park <william.h.park@gmail.com>
2021-01-16 17:33:32 -0800

    update

--- src/public/js/profile.js
+++ src/public/js/profile.js
@@ -1,10 +1,12 @@
+const testData = require("../../test");
+
 $(document).ready(function () {
   $("#submitFacebookProfile").on("click", function (event) {
     event.preventDefault();
     $.ajax({
       url: `${window.location.origin}/set-up-user-fb-profile`,
       method: "POST",
-      data: {},
+      data: { testData },
       success: function (data) {
         alert("Setup succeeds");
         console.log(data);
