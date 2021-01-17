c2843ab8bd5f55ac5b0a600a0606a2cfc785d55e
William Park <william.h.park@gmail.com>
2021-01-17 01:07:14 -0800

    update

--- src/services/chatBotService.js
+++ src/services/chatBotService.js
@@ -2,6 +2,7 @@ const request = require("request");
 require("dotenv").config();
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const testData = require("../../testData");
 
 let sendMessage = (sender_psid, response) => {
   let request_body = {
