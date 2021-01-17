0a6b18e6ab416721a6da99ce283f03106bc7ada4
William Park <william.h.park@gmail.com>
2021-01-16 16:51:03 -0800

    update

--- src/services/chatBotService.js
+++ src/services/chatBotService.js
@@ -240,4 +240,5 @@ module.exports = {
   sendResponseWelcomeNewCustomer: sendResponseWelcomeNewCustomer,
   sendMainMenu: sendMainMenu,
   createResponse: createResponse,
+  sendRestaurant: sendRestaurant,
 };
